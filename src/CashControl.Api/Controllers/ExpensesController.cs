f777c26fa92261a235d8309524f9f6610b078630
Rafael Brito <henrique8264@gmail.com>
2024-07-24 19:02:56 -0300

    feat: Implementar controller para registrar uma despesa.

---
+++ src/CashControl.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,14 @@
+﻿using CashControl.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashControl.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        return Created();
+    }
+}
---
+++ src/CashControl.Communication/Requests/RequestRegisterExpenseJson.cs
@@ -0,0 +1,11 @@
+﻿using CashControl.Communication.Enums;
+
+namespace CashControl.Communication.Requests;
+public class RequestRegisterExpenseJson
+{
+    public string Title { get; set; } = String.Empty;
+    public string Description { get; set; } = String.Empty;
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+    public PaymentType PaymentType { get; set; }
+}
