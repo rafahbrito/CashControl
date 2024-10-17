eb3d0d837c76d851be2700d7eee55e447c730ed4
Rafael Brito <henrique8264@gmail.com>
2024-10-17 20:01:23 -0300

    test: Add RegisterExpenseValidator success case test

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using CashControl.Communication.Enums;
+using CashControl.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {
+        return new Faker<RequestRegisterExpenseJson>()
+            .RuleFor(req => req.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(req => req.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(req => req.Date, faker => faker.Date.Past())
+            .RuleFor(req => req.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(req => req.Amount, faker => faker.Random.Decimal(min: 1, max: 1000));
+    }
+}
---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,20 @@
+﻿using CashControl.Application.UseCases.Expenses.Register;
+using CommonTestUtilities.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+}
